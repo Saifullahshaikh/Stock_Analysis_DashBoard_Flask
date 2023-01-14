fc5c9c4c32ff7878623a52c00192923aa36a3dac
SAIFULLAH SHAIKH <ssaifullah272@gmail.com>
2023-01-14 21:30:10 +0500

    Readme file added

---
+++ Readme.txt
@@ -0,0 +1,19 @@
+Steps to run the app.
+Step 1: Unzip the folder.
+
+Step 2: Install Python 3.11 (if not installed)
+
+Step 3: open the unziped folder in command prompt(cmd) or any code editor terminal.
+
+Step 4: Activate the virtual envirnment with 
+	command 'venv/Scripts/activate' and press enter.
+
+Step 5: Install the required packages.
+	type command 'pip install flask' and press enter.
+	type command 'pip install yfinance' and press enter.
+	type command 'pip install matplotlib' and press enter.
+
+Step 8: Run the app with
+	command 'py init.py' and press enter.
+
+Step 9: Copy the URL and Paste in web Browser.
--- init.py
+++ init.py
@@ -39,23 +39,6 @@ def get_plot():
         p3 = plot(key,pur,sale1,sale2,"1mo")
         p4 = plot(key,pur,sale1,sale2,"1y")
         p5 = plot(key,pur,sale1,sale2,"max")
-        '''key.upper()
-        st_key = yfinance.Ticker(key)
-        hist = st_key.history(period="1y")
-        items= len(hist.index)
-        purchase = [int(pur)]*items
-        sale_val1= [int(sale1)]*items
-        sale_val2 = [int(sale2)]*items
-        plt.plot(hist.index, hist['Close'], label= "Original Price")
-        plt.plot(hist.index, purchase, label= "Purchase Price")
-        # plotting the line 2 points 
-        plt.plot(hist.index, sale_val1,  label= "Sale Price 1")
-        plt.plot(hist.index, sale_val2, label= "Sale Price 2")
-        # giving a title to my graph
-        plt.title("1y")
-        # show a legend on the plot
-        #plt.legend("Actual Price","Purchase Price", "Sale Price 1", "Sale Price 2")
-        plt.savefig('static/myplot.png')'''
         return render_template('index.html',plot_url1=str(p1),plot_url2=str(p2),plot_url3=str(p3),plot_url4=str(p4),plot_url5=str(p5))
     else:
         return render_template('index.html')
